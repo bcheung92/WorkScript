140ede84ddea116beb4530a3ae4c740dc056c0d6
bhcueng92 <339526286@qq.com>
2017-01-07 16:07:58 +0800

    script modified, combine the script8_thread.py and trans.py

--- script8_thread_modified.py
+++ script8_thread_modified.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
 ### this is the python script created by Ferris Cheung 2017-01-01
-### It is used to get the shared Cache Reuse Distance Distribution which is added up by diff core's RDD-----L2R,and it's output as name-reuse.txt
+### It is used to get the shared Cache Reuse Distance Distribution which is added up by diff core's RDD-----L2RD
+### and then we trans she l2RD to l2R,according to int trans the first element of L2RD
 ### and it gets the shared Cache Stack Distance Distribution which is collected in the l2 cache port ----L2E,and it's output as name-stack.txt
 ### This Script is the to get the data from the same thread
 
@@ -71,15 +72,32 @@ debug = 1
 threadnum = 1
 #stats num
 statsnum = 1
+
+# task lines is the line read of system.tasks.txt
 tasklines = tkRead.readline()
+
+###
+###  func to get the num length list
+###
 def listget(num):
     ret = []
     for i in range(num):
         ret.append(0)
     return ret
+###
+###
+def addlist(num,a):
+    for i in range(num):
+        a.append([])
+    return a
 
-rdaddup = listget(200)
+
+### rdaddup means reuse distance addup , this list is the sum of reuse distance distribution 
+rdaddup = listget(800)
+
+### sdaddup means stack distance addup , this list is the sum of stack distance distribution
 sdaddup = listget(30)
+
 ###
 ### func check is mean to check the distribution is continuous or not
 ###
@@ -90,12 +108,29 @@ def check(a,b):
         c = b-a
         return c
 
+###
+### func to get the former num a element summary
+###
+def littsum(a,b):
+    ret = 0
+    for i in range(a):
+        ret =ret + b[i]
+    return ret
+
 while tasklines:
+    ### check if this thread is the thread we want or not
     threadnamematch = threadnamePattern.search(tasklines)
     if threadnamematch:
+        # if match means we need to collect the thread info
         while 1:
             if threadnum == statsnum:
+                # to arrive the threadinfo in the stats.txt
+                # if equal means we alrady arrived
                 print threadnum
+                # this flag to denote we enter the thread info ,and we should collect the data
+                # when we the num equal ,we set it to false 
+                # when we enter the thread we set it to true
+                # and when we collect the data finished, we used it to checn if to break or not
                 threadflag = False
                 ##du stats data
                 while lines:
@@ -117,7 +152,11 @@ while tasklines:
                         core0pos=1
                         core1pos=1
                         l2pos = 1
-                        l2R = {}
+                        ## the l2RD means the l2R dict , it used to get the changed reuse distance
+                        ## the l2R means the changed and transed l2 reuse distance 
+                        ## the l2ES is the l2 expected stack disance distribution
+                        l2RD = {}
+                        l2R = []
                         l2ES=[]
                         #this three pos is the pointer of the Distribution we are collecting
                         pos0=0
@@ -126,6 +165,7 @@ while tasklines:
                         threadlines = fpRead.readline()
                         threadendmatch = threadendPattern.match(threadlines)
                         while threadlines:
+                            # enter the thread phase, set the match
                             core0Dismatch = core0ReuseDisPattern.search(threadlines)
                             core1Dismatch = core1ReuseDisPattern.search(threadlines)
                             core0samplematch = core0ReuseSamplePattern.search(threadlines)
@@ -227,6 +267,7 @@ while tasklines:
                                 else:
                                     cpi1=0
                             if threadendmatch:
+                                # the thread collect had reach the end
                                 statsnum = statsnum+1
                                 #full the histogram
                                 dis0 = check(pos0,300)
@@ -270,22 +311,39 @@ while tasklines:
                                     temp1 = (fac0/fac1)*cpic1
                                     core0coe = float('%.2f'%temp0)
                                     core1coe = float('%.2f'%temp1)
-                                    for i in range(300):
+                                    ## to comfirm the l2R's size
+                                    l2R = listget(max(int(300*(1+core0coe)),int(300*(1+core1coe))))
+                                    ## in this part ,we calc the L2RD,which is the reuse distance just addup
+                                    ## notice that the core0coe is the cofficient of core0 
+                                    ## and the cofficient should be worked at the distance,
+                                    ## it cannot be worked at the num of distance
+                                    for i in range(301):
+                                        ## skip when i==0
+                                        if i==0:
+                                            continue
                                         pm1 = i*(1+core0coe)
                                         pm2 = i*(1+core1coe)
-                                        l2R[pm1]=core0[i]
-                                        l2R[pm2]=core1[i]
+                                        l2RD[pm1]=core0[i]
+                                        l2RD[pm2]=core1[i]
                                 elif cpi1==0:
+                                    l2R = listget(300)
                                     for j1 in range(300):
-                                        l2R[j1]=core0[j1]
+                                        l2RD[j1+1]=core0[j1]
                                 elif cpi0==0:
+                                    l2R = listget(300)
                                     for j2 in range(300):
-                                        l2R[j2]=core1[j2]
+                                        l2RD[j2+1]=core1[j2]
+                                ## in this part we do the int trans for l2RD dict
+                                for i in l2RD.items():
+                                    temp = int(i[0])-1
+                                    l2R[temp]=l2R[temp]+i[1]
+                                ## add the distribution,get sdaddup and rdaddup
                                 for s1 in range(30):
                                     sdaddup[s1]=sdaddup[s1]+l2[s1]
-                                for s2 in range(200):
+                                for s2 in range(len(l2R)):
                                     rdaddup[s2]=rdaddup[s2]+l2R[s2]
-                                for k in range(300):
+                                ## write the 
+                                for k in range(len(l2R)):
                                     RDHWrite.write("%d " %l2R[k])
                                 for m in range(30):
                                     SDHWrite.write("%d " %l2[m])
@@ -315,12 +373,44 @@ while tasklines:
 
     tasklines=tkRead.readline()
 
+##
+distance = [1]
+distance = addlist(len(rdaddup),distance)
+rdsum = sum(rdaddup)
+##to trans the l2 reuse distance to the l2 expect stack distance 
+for i in range(len(rdaddup)):
+    pos = i+1
+    if(i==len(l2R)):
+        break
+    distance[pos] = sd[i]+((rdsum - littsum(i))/float(rdsum))
+## adjust the l2 expect stack distance 
+adjust = listget(len(distance),adjust)
+for i in range(len(distance)):
+    if(distance[i]>int(distance[i])+0.5):
+        adjust[i]=distance[i]+1
+    else:
+        adjust[i]=int(distance[i])
+## according to the adjust stack distance we set up the expect stack distance distribution
+esd=[]
+esd = listget(max(adjust))
+for i in range(len(rdaddup)):
+    esd[adjust[i]-1]=esd[adjust[i]-1]+rdaddup[i]
+
+## to calc the hit num according to the esd
+num = 0
+for i in range(len(adjust)):
+    if adjust[i]<=8:
+        num = num+1
+
+esdhit = littsum(num,rdaddup)
+sdhit = listsum(8,sdaddup)
 for d1 in range(30):
     SD8Write.write("%d " %sdaddup[d1])
-SD8Write.write("%d " % sum(sdaddup))
-for d2 in range(200):
-    RD8Write.write("%d " %rdaddup[d2])
-RD8Write.write("%d "% sum(rdaddup))
+SD8Write.write("%d " % sdhit)
+
+for d2 in len(esd):
+    RD8Write.write("%d " %esd[d2])
+RD8Write.write("%d "% esdhit)
 
 tkRead.close()
 fpRead.close()
